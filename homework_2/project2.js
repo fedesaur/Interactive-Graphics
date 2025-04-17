778ee488702a969eff72e5ed6db98f22c3fa216d
fedesaur <104363014+fedesaur@users.noreply.github.com>
2025-04-17 10:43:52 +0200

    Update project2.js

--- homework_2/project2.js
+++ homework_2/project2.js
@@ -3,7 +3,20 @@
 // The given rotation value is in degrees.
 function GetTransform( positionX, positionY, rotation, scale )
 {
-	return Array( 1, 0, 0, 0, 1, 0, 0, 0, 1 );
+    let rad = rotation * Math.PI / 180;
+    let cos = Math.cos(rad);
+    let sin = Math.sin(rad);
+
+    let m00 = scale * cos;
+    let m01 = -scale * sin;
+    let m10 = scale * sin;
+    let m11 = scale * cos;
+
+    return [
+        m00, m10, 0,  
+        m01, m11, 0,  
+        positionX, positionY, 1 
+	]
 }
 
 // Returns a 3x3 transformation matrix as an array of 9 values in column-major order.
@@ -11,5 +24,17 @@ function GetTransform( positionX, positionY, rotation, scale )
 // The returned transformation first applies trans1 and then trans2.
 function ApplyTransform( trans1, trans2 )
 {
-	return Array( 1, 0, 0, 0, 1, 0, 0, 0, 1 );
+	let result = new Array(9);
+
+    for (let row = 0; row < 3; row++) {
+        for (let col = 0; col < 3; col++) {
+            result[col * 3 + row] =
+                trans1[0 * 3 + row] * trans2[col * 3 + 0] +
+                trans1[1 * 3 + row] * trans2[col * 3 + 1] +
+                trans1[2 * 3 + row] * trans2[col * 3 + 2];
+        }
+    }
+
+    return result;
 }
+
